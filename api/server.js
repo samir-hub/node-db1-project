164e2361982fc87993b321b76bec4fc59a2e426a
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-04-08 22:35:07 -0400

    done with MVP

--- api/server.js
+++ api/server.js
@@ -6,4 +6,82 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  db.select()
+    .from("accounts")
+    .then((accounts) => {
+      res.json(accounts);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error retrieving accounts", err });
+    });
+});
+
+server.get("/:id", (req, res) => {
+  db.select()
+    .from("accounts")
+    .where({ id: req.params.id })
+    .first()
+    .then((account) => {
+      if (account) {
+        res.json(account);
+      } else {
+        res.status(400).json({ message: "Account not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Error retrieving account", err });
+    });
+});
+
+server.post("/", (req, res) => {
+  const accountData = req.body;
+
+  db("accounts")
+    .insert(accountData)
+    .then(() => {
+      res.status(201).json({ message: "Account created successfully" });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create account", err });
+    });
+});
+
+server.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db("accounts")
+    .where({ id })
+    .update(changes)
+    .then((account) => {
+      if (account) {
+        res.json({ message: "Account updated successfully" });
+      } else {
+        res.status(404).json({ message: "Invalid ID" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to update account", err });
+    });
+});
+
+server.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db("accounts")
+    .where({ id })
+    .delete()
+    .then((account) => {
+      if (account) {
+        res.json({ message: "Account deleted successfully" });
+      } else {
+        res.status(404).json({ message: "Invalid ID" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to delete account", err });
+    });
+});
+
 module.exports = server;
